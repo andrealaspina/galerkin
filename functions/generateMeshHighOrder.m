b081c9adccafb02704ff87c467b62a324204f2e3
Andrea La Spina <andrea.la-spina@tum.de>
2025-01-10 17:03:32 +0100

    Speed up generateMeshHighOrder() for large meshes by preallocating Xa_add

--- functions/generateMeshHighOrder.m
+++ functions/generateMeshHighOrder.m
@@ -7,31 +7,34 @@ Cg=MeshLow.Elements';
 Xg=MeshLow.Nodes';
 nsd=size(MeshLow.Nodes,1);
 NumElements=size(MeshLow.Elements,2);
-NumElementNodesAppr=size(RefElementApproxim.NodesCoordElem,1);
+NumElementNodes1=nsd+1;
+NumElementNodesApp=size(RefElementApproxim.NodesCoordElem,1);
+NumElementNodesAdd=NumElementNodesApp-NumElementNodes1;
 
 % Linear mesh
-C1=Cg(:,1:(nsd+1));
-Nmax1=length(unique(C1));
-X1=Xg(1:Nmax1,:);
+C1=Cg(:,1:NumElementNodes1);
+N1=length(unique(C1));
+X1=Xg(1:N1,:);
 
 % Get shape functions
 Nga=RefElementGeometry.ShapeFunctionsElem;
 Naa=RefElementApproxim.ShapeFunctionsElem;
 
 % Insert additional nodes connectivity and coordinates
-Xa_add=[];
+Xa_add=cell(NumElements,1);
 parfor iElem=1:NumElements
   Cge=Cg(iElem,:);
   Xge=Xg(Cge,:); %#ok
   Xae=Naa\(Nga*Xge);
-  Xa_add=[Xa_add;Xae((nsd+2):end,:)];
+  Xa_add{iElem}=Xae(NumElementNodes1+1:end,:);
 end
+Xa_add=vertcat(Xa_add{:});
 
 % Condense shared nodes
 tol=1e-9;
 [~,ind1,ind2]=uniquetol(double(Xa_add),tol,'ByRows',true);
 Xa_add_unique=Xa_add(ind1,:);
-Ca_add_unique=reshape(Nmax1+ind2,[NumElementNodesAppr-(nsd+1),NumElements])';
+Ca_add_unique=reshape(N1+ind2,[NumElementNodesAdd,NumElements])';
 
 % Merge nodes of linear mesh with nodes of high-order mesh
 Xa=[X1;Xa_add_unique];
