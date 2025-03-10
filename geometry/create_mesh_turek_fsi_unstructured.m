d4ee19b261b1d17b2e6e9a782a59765532d15839
Andrea La Spina <andrealaspina.galerkin@gmail.com>
2025-03-10 19:02:29 +0100

    Removed personal path

--- geometry/create_mesh_turek_fsi_unstructured.m
+++ geometry/create_mesh_turek_fsi_unstructured.m
@@ -1,14 +1,13 @@
 close all; clear; clc;
 
 % Import mesh (fluid)
-[Mesh(1)]=importGmshMesh('/Users/andrealaspina/Desktop/galerkin/geometry/turek_fsi_unstructured_fluid');
+[Mesh(1)]=importGmshMesh('turek_fsi_unstructured_fluid');
 
 % Import mesh (ALE)
-[Mesh(2)]=importGmshMesh('/Users/andrealaspina/Desktop/galerkin/geometry/turek_fsi_unstructured_mesh');
+[Mesh(2)]=importGmshMesh('turek_fsi_unstructured_mesh');
 
 % Import mesh (structure)
-[Mesh(3)]=importGmshMesh('/Users/andrealaspina/Desktop/galerkin/geometry/turek_fsi_unstructured_structure');
+[Mesh(3)]=importGmshMesh('turek_fsi_unstructured_structure');
 
 % Save mesh
-save('/Users/andrealaspina/Desktop/galerkin/geometry/Mesh_turek_fsi_unstructured.mat','Mesh');
-fprintf('\nMesh_turek_fsi_unstructured.mat completed');
+save('Mesh_turek_fsi_unstructured.mat','Mesh');
