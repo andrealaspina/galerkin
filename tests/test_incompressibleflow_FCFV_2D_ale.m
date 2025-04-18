b8af892e718f74dd19a8efa60107794112d5296c
Andrea La Spina <andrealaspina.galerkin@gmail.com>
2025-04-18 17:04:02 +0200

    Added ALE tests for FCFV

---
+++ tests/test_incompressibleflow_FCFV_2D_ale.m
@@ -0,0 +1,120 @@
+clearvars -except Files Passed CPUTime Test;
+
+%% Input
+
+% Simulation ---------------------------------------------------------------------------------------
+Simulation.Type='SingleSimulation';              % Simulation type
+Simulation.Problem='FluidALE';                   % Problem
+% --------------------------------------------------------------------------------------------------
+
+% Parameters ---------------------------------------------------------------------------------------
+Parameters(1).Formulation='IncompressibleFlow_FCFV';% Formulation
+Parameters(1).Problem='Fluid';                   % Problem
+Parameters(1).PostProcessingHDG='no';            % Perform HDG postprocessing
+Parameters(1).ConvectiveFlow='yes';              % Convective flow
+Parameters(1).ArbitraryLagrangianEulerian='yes'; % Arbitrary Lagrangian-Eulerian description
+Parameters(1).Degree=0;                          % Degree
+Parameters(1).StabVelocity=1;                    % Stabilization for velocity
+Parameters(1).StabPressure=1;                    % Stabilization for pressure
+Parameters(1).Density=1;                         % Density
+Parameters(1).DynamicViscosity=0.1;              % Dynamic viscosity
+r=Parameters(1).Density; mu=Parameters(1).DynamicViscosity;
+Parameters(1).ScaledStrainRate=...               % Scaled strain rate
+  @(x,y,z,t) [-2^(1/2)*mu^(1/2)*pi*cos(pi*x).*sin(pi*y)*sin(pi*t),...
+              +2^(1/2)*mu^(1/2)*pi*cos(pi*x).*sin(pi*y)*sin(pi*t),...
+              0*x];
+Parameters(1).Velocity=...                       % Velocity
+  @(x,y,z,t) [sin(pi.*t).*sin(pi.*x).*sin(pi.*y),...
+              cos(pi.*x).*cos(pi.*y).*sin(pi.*t)];
+Parameters(1).Pressure=...                       % Pressure
+  @(x,y,z,t) pi.*cos(pi.*x).*sin(pi.*t).*sin(pi.*y);
+Parameters(1).Traction=...                       % Traction
+  @(x,y,z,t) [0*x, 0*x];
+Parameters(1).Force=...                          % Force
+  @(x,y,z,t) [pi.*sin(pi.*x).*(+r.*cos(pi.*x)+r.*cos(pi.*t).*sin(pi.*y)-pi.*sin(pi.*t).*sin(pi.*y)-r.*cos(pi.*t)^2.*cos(pi.*x)+2.*mu.*pi.*sin(pi.*t).*sin(pi.*y)),...
+              pi.*cos(pi.*y).*(-r.*sin(pi.*y)+r.*cos(pi.*t).*cos(pi.*x)+pi.*cos(pi.*x).*sin(pi.*t)+r.*cos(pi.*t)^2.*sin(pi.*y)+2.*mu.*pi.*cos(pi.*x).*sin(pi.*t))];
+Parameters(1).ScaledStrainRateCenter=...         % Scaled strain rate at the center
+  @(t) Parameters(1).ScaledStrainRate(0.5,0.5,0,t);
+Parameters(1).VelocityCenter=...                 % Velocity at the center
+  @(t) Parameters(1).Velocity(0.5,0.5,0,t);
+Parameters(1).PressureCenter=...                 % Pressure at the center
+  @(t) Parameters(1).Pressure(0.5,0.5,0,t);
+Parameters(2).Formulation='Elasticity_CG';       % Formulation
+Parameters(2).Problem='Mesh';                    % Problem
+Parameters(2).Model='LinearElasticity';          % Model
+Parameters(2).Assumption='PlaneStrain';          % Assumption
+Parameters(2).Degree=1;                          % Degree
+Parameters(2).NitschePenalty=1000;               % Nitsche's penalty parameter
+Parameters(2).Density=1;                         % Density
+Parameters(2).YoungsModulus=@(x,y,z) 5/4;        % Young's modulus
+Parameters(2).PoissonsRatio=@(x,y,z) 1/4;        % Poisson's ratio
+Parameters(2).Displacement=...                   % Displacement
+  @(x,y,z,t) [1/4*sin(2*pi*x).*(1-cos(2*pi*y)).*(1-cos(2*pi*t))*1/8,...
+              1/4*sin(2*pi*y).*(1-cos(2*pi*x)).*(1-cos(2*pi*t))*1/8];
+Parameters(2).Traction=@(x,y,z,t) [0*x,0*x];     % Traction
+Parameters(2).Force=...                          % Force
+  @(x,y,z,t) [-pi^2/16*sin(2*pi*x).*(6*cos(2*pi*y)+cos(2*pi*t)-4*cos(2*pi*y)*cos(2*pi*t)-3),...
+              -pi^2/16*sin(2*pi*y).*(6*cos(2*pi*x)+cos(2*pi*t)-4*cos(2*pi*x)*cos(2*pi*t)-3)];
+Parameters(2).DisplacementCenter=...             % Displacement at the center
+  @(t) Parameters(2).Displacement(0.5,0.5,0,t);
+clear r mu
+% --------------------------------------------------------------------------------------------------
+
+% Geometry and mesh --------------------------------------------------------------------------------
+Mesh.File={'Mesh_square01_ale_2'};               % Mesh file
+% --------------------------------------------------------------------------------------------------
+
+% System -------------------------------------------------------------------------------------------
+System.SymmetrizeMatrix='no';                    % Symmetrize matrix
+System.Nonlinear='yes';                          % Nonlinear
+System.Tolerance=1e-9;                           % Tolerance
+System.MaxIterations=100;                        % Maximum number of iterations
+% --------------------------------------------------------------------------------------------------
+
+% Time parameters ----------------------------------------------------------------------------------
+Time.TimeDependent='yes';                        % Time dependent problem
+Time.InitialTime=0;                              % Initial time
+Time.FinalTime=0.5;                              % Final time
+Time.TimeStepSize=2^(-3);                        % Time step size
+Time.BDFOrder=4;                                 % BDF order
+% --------------------------------------------------------------------------------------------------
+
+% Solver -------------------------------------------------------------------------------------------
+Solver.Type='backslash';                         % Type
+% --------------------------------------------------------------------------------------------------
+
+% Boundary splitting -------------------------------------------------------------------------------
+Boundaries(1).Dirichlet_v_x=[1,2,3,4];           % Dirichlet portion for x-velocity
+Boundaries(1).Dirichlet_v_y=[1,2,3,4];           % Dirichlet portion for y-velocity
+Boundaries(1).Dirichlet_p=[1,2,3,4];             % Dirichlet portion for pressure
+Boundaries(1).Neumann_t_x=[];                    % Neumann portion for x-traction
+Boundaries(1).Neumann_t_y=[];                    % Neumann portion for y-traction
+Boundaries(2).Dirichlet=[1,2,3,4];               % Dirichlet portion
+Boundaries(2).Neumann=[];                        % Neumann portion
+% --------------------------------------------------------------------------------------------------
+
+% Output options -----------------------------------------------------------------------------------
+Options.PlotGeometry='no';                       % Plot geometry
+Options.PlotMesh='no';                           % Plot mesh
+Options.ComputeError=...                         % Compute error
+  {'ScaledStrainRateCenter','Number';
+   'VelocityCenter'        ,'Number';
+   'PressureCenter'        ,'Number';
+   'DisplacementCenter'    ,'Number'};
+Options.ComputeQuantityError=...                 % Compute quantity of interest (error computation)
+  ['[~,Node1]=ismember([0.5,0.5,0],Mesh(1).Nodes'',''rows''); ',...
+   '[~,Node2]=ismember([0.5,0.5,0],Mesh(2).Nodes'',''rows''); ',...
+   'Results(1).ScaledStrainRateCenter=Results(1).ScaledStrainRate(Node1,:); ',...
+   'Results(1).VelocityCenter=Results(1).Velocity(Node1,:); ',...
+   'Results(1).PressureCenter=Results(1).Pressure(Node1,:); ',...
+   'Results(2).DisplacementCenter=Results(2).Displacement(Node2,:);'];
+Options.Test=...                                 % Test
+  ['abs(Results(1).ScaledStrainRateCenterErrorNumber-6.257078621031293e-01)<1e-12 && ',...
+   'abs(Results(1).VelocityCenterErrorNumber        -5.383729373024970e-01)<1e-12 && ',...
+   'abs(Results(1).PressureCenterErrorNumber        -1.058816192322839e-00)<1e-12 && ',...
+   'abs(Results(2).DisplacementCenterErrorNumber    -3.659813721148478e-17)<1e-12'];
+% --------------------------------------------------------------------------------------------------
+
+%% Main
+
+main
--- tests/test_incompressibleflow_FCFV_2D_ale_imposed.m
+++ tests/test_incompressibleflow_FCFV_2D_ale_imposed.m
@@ -12,7 +12,7 @@ Parameters.Formulation='IncompressibleFlow_FCFV';% Formulation
 Parameters.Problem='Fluid';                      % Problem
 Parameters.PostProcessingHDG='no';               % Perform HDG postprocessing
 Parameters.ConvectiveFlow='yes';                 % Convective flow
-Parameters.ArbitraryLagrangianEulerian='no';     % Arbitrary Lagrangian-Eulerian description
+Parameters.ArbitraryLagrangianEulerian='yes';    % Arbitrary Lagrangian-Eulerian description
 Parameters.Degree=0;                             % Degree
 Parameters.StabVelocity=1;                       % Stabilization for velocity
 Parameters.StabPressure=1;                       % Stabilization for pressure
@@ -33,11 +33,20 @@ Parameters.Traction=...                          % Traction
 Parameters.Force=...                             % Force
   @(x,y,z,t) [pi.*sin(pi.*x).*(+r.*cos(pi.*x)+r.*cos(pi.*t).*sin(pi.*y)-pi.*sin(pi.*t).*sin(pi.*y)-r.*cos(pi.*t)^2.*cos(pi.*x)+2.*mu.*pi.*sin(pi.*t).*sin(pi.*y)),...
               pi.*cos(pi.*y).*(-r.*sin(pi.*y)+r.*cos(pi.*t).*cos(pi.*x)+pi.*cos(pi.*x).*sin(pi.*t)+r.*cos(pi.*t)^2.*sin(pi.*y)+2.*mu.*pi.*cos(pi.*x).*sin(pi.*t))];
+Parameters.Displacement=...                      % Displacement
+  @(x,y,z,t) [1/4*sin(2*pi*x).*(1-cos(2*pi*y)).*(1-cos(2*pi*t))*1/8,...
+              1/4*sin(2*pi*y).*(1-cos(2*pi*x)).*(1-cos(2*pi*t))*1/8];
+Parameters.ScaledStrainRateCenter=...            % Scaled strain rate at the center
+  @(t) Parameters.ScaledStrainRate(0.5,0.5,0,t);
+Parameters.VelocityCenter=...                    % Velocity at the center
+  @(t) Parameters.Velocity(0.5,0.5,0,t);
+Parameters.PressureCenter=...                    % Pressure at the center
+  @(t) Parameters.Pressure(0.5,0.5,0,t);
 clear r mu
 % --------------------------------------------------------------------------------------------------
 
 % Geometry and mesh --------------------------------------------------------------------------------
-Mesh.File={'Mesh_square01_1'};                   % Mesh file
+Mesh.File={'Mesh_square01_2'};                   % Mesh file
 % --------------------------------------------------------------------------------------------------
 
 % System -------------------------------------------------------------------------------------------
@@ -71,13 +80,18 @@ Boundaries.Neumann_t_y=[];                       % Neumann portion for y-tractio
 Options.PlotGeometry='no';                       % Plot geometry
 Options.PlotMesh='no';                           % Plot mesh
 Options.ComputeError=...                         % Compute error
-  {'ScaledStrainRate';
-   'Velocity';
-   'Pressure'};
+  {'ScaledStrainRateCenter','Number';
+   'VelocityCenter'        ,'Number';
+   'PressureCenter'        ,'Number'};
+Options.ComputeQuantityError=...                 % Compute quantity of interest (error computation)
+  ['[~,Node]=ismember([0.5,0.5,0],Mesh.Nodes'',''rows''); ',...
+   'Results.ScaledStrainRateCenter=Results.ScaledStrainRate(Node,:); ',...
+   'Results.VelocityCenter=Results.Velocity(Node,:); ',...
+   'Results.PressureCenter=Results.Pressure(Node,:);'];
 Options.Test=...                                 % Test
-  ['abs(Results.ScaledStrainRateErrorL2-5.981136636295634e-01)<1e-12 && ',...
-   'abs(Results.VelocityErrorL2        -2.532299856794472e-01)<1e-12 && ',...
-   'abs(Results.PressureErrorL2        -5.193011528487051e-01)<1e-12'];
+  ['abs(Results.ScaledStrainRateCenterErrorNumber-6.082459475196617e-01)<1e-12 && ',...
+   'abs(Results.VelocityCenterErrorNumber        -5.692053686573615e-01)<1e-12 && ',...
+   'abs(Results.PressureCenterErrorNumber        -1.080293813337339e-00)<1e-12'];
 % --------------------------------------------------------------------------------------------------
 
 %% Main
